974cebc985fab66430fd6cbaedac19a6c04abb5a
Bogdan <Bogdan.drzewiecki@smcebi.edu.pl>
2016-10-24 11:40:08 +0200

    dodalem enter

--- srednia_kwadratowa.py
+++ srednia_kwadratowa.py
@@ -1,6 +1,11 @@
 import math
 import myerror
 def srednia_kwadratowa(liczby):
+	"""
+	Funkcja liczaca srednia kwadratowa
+	
+	liczby - list,tuple
+	"""
 	if type(liczby) != list and type(liczby) != tuple:
 		raise myerror.IBeBack()
 	for i in range(len(liczby)):
